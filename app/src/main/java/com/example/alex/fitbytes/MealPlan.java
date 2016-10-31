6d410d58ba29d01a4cfce2ac6ca1805c0565c113
Alex Her <her.alex@live.com>
2016-10-31 11:03:07 -0500

    Can now add dates to the list of upcoming plans.

--- app/src/main/java/com/example/alex/fitbytes/MealPlan.java
+++ app/src/main/java/com/example/alex/fitbytes/MealPlan.java
@@ -1,21 +1,30 @@
 package com.example.alex.fitbytes;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class MealPlan extends AppCompatActivity {
 
     private String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
     private int currentMonth;
     private Integer numbers[] = new Integer[7];
-    private String[] mealList = {"asdfsadfsadf", "dasfasdf", "asdfsadf","asdfsadfsadf", "dasfasdf", "asdfsadf","asdfsadfsadf", "dasfasdf", "asdfsadf"};
+    private List<String> mealList;
+    private String month;
+    private String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +32,38 @@ public class MealPlan extends AppCompatActivity {
         setContentView(R.layout.activity_meal_plan);
         Calendar date = Calendar.getInstance();
 
-//        SimpleDateFormat month_date = new SimpleDateFormat("MM");
-//        String month_name = month_date.format(date.getTime());
+        SimpleDateFormat month_date = new SimpleDateFormat("MMM");
+        month = month_date.format(date.getTime());
         currentMonth = date.get(Calendar.MONTH); //NOTE: Calendar.MONTH is zero based
 
         for (int i = 0; i < 7; i++) {
             numbers[i] = date.get(Calendar.DAY_OF_MONTH);
             date.add(Calendar.DAY_OF_MONTH, 1);
         }
-
+        mealList = new ArrayList<String>();
+        mealList.add("Oct 19");
+        mealList.add("Oct 25");
+        mealList.add("Nov 4");
         createDropdowns();
         createMeals();
+
+        Button addButton = (Button)findViewById(R.id.addMeal);
+        addButton.setOnClickListener(new AdapterView.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                mealList.add(makeMealPlan());
+                MealPlan.this.createMeals();
+
+                // Displays a message
+                Context context = getApplicationContext();
+                CharSequence text = "Meal Plan Added";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
     }
 
     private void createDropdowns() {
@@ -42,14 +72,60 @@ public class MealPlan extends AppCompatActivity {
         monthDropdown.setAdapter(dayAdapter);
         monthDropdown.setSelection(currentMonth);
 
+        // Listener for month dropdown
+        monthDropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                MealPlan.this.setMonth(parent.getSelectedItem().toString());
+//                Log.d("GET ID", Long.toString(id)); // Maybe use this to make a Date type
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
         Spinner numDropdown = (Spinner)findViewById((R.id.daySpinner));
         ArrayAdapter<Integer> numAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_dropdown_item, numbers);
         numDropdown.setAdapter(numAdapter);
+        // Listener for date dropdown
+        numDropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                MealPlan.this.setDate(parent.getSelectedItem().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     private void createMeals(){
         ListView lv = (ListView)findViewById(R.id.plannedMeals);
         ArrayAdapter<String> myarrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mealList);
         lv.setAdapter(myarrayAdapter);
+    }
+
+    public void setMonth(String m){
+        month = m;
+    }
+
+    public String getMonth(){
+        return month;
+    }
+
+    public void setDate(String d){
+        date = d;
+    }
+
+    public String getDate(){
+        return date;
+    }
+
+    public String makeMealPlan(){
+        return month + " " + date;
     }
 }
