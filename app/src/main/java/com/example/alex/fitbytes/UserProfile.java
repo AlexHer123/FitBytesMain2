2b49a040792b582c740bd495527455d3cff5c9aa
scaffi29 <scaffi29@uwm.edu>
2016-11-02 17:38:51 -0500

    userProfile data and methods

--- app/src/main/java/com/example/alex/fitbytes/UserProfile.java
+++ app/src/main/java/com/example/alex/fitbytes/UserProfile.java
@@ -15,4 +15,41 @@ public class UserProfile  extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes);
     }
+
+
+    int height, weight;
+    double BMI;
+    boolean isHealthyBMI;
+
+    int healthyBMImin = 19;
+    int healthyBMImax = 25;
+    int BMIconversionFactor = 703;
+
+
+    public UserProfile(){
+         height = 70;
+         weight = 180;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setHeight(int h) {
+        height = h;
+    }
+
+    public void setWeight(int w) {
+        weight = w;
+    }
+
+    public void calculateBMI(){
+        BMI = BMIconversionFactor*(weight/(height*height));     // 703*weight/height^2
+        if (BMI > healthyBMImin && BMI < healthyBMImax)
+            isHealthyBMI = true;
+    }
 }
