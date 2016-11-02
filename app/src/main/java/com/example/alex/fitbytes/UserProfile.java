6527b7c64c9e14bcf5b948e8ca0c9cd434a3efa4
scaffi29 <scaffi29@uwm.edu>
2016-11-02 18:23:03 -0500

    userProfile layout

--- app/src/main/java/com/example/alex/fitbytes/UserProfile.java
+++ app/src/main/java/com/example/alex/fitbytes/UserProfile.java
@@ -2,7 +2,8 @@ package com.example.alex.fitbytes;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 /**
@@ -10,11 +11,7 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class UserProfile  extends AppCompatActivity{
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipes);
-    }
+
 
 
     int height, weight;
@@ -25,11 +22,25 @@ public class UserProfile  extends AppCompatActivity{
     int healthyBMImax = 25;
     int BMIconversionFactor = 703;
 
+    EditText heightEditText = (EditText)findViewById(R.id.userProfileUserHeight);
+    EditText weightEditText = (EditText)findViewById(R.id.userProfileUserWeight);
+    TextView BMItextView = (TextView)findViewById(R.id.userProfileBMI);
 
-    public UserProfile(){
-         height = 70;
-         weight = 180;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+
+        height = 70;
+        weight = 180;
+        calculateBMI();
+
+        heightEditText.setText(""+height, TextView.BufferType.EDITABLE);
+        weightEditText.setText(""+weight, TextView.BufferType.EDITABLE);
+        BMItextView.setText(""+BMI, TextView.BufferType.EDITABLE);
+
     }
+
 
     public int getHeight() {
         return height;
@@ -47,7 +58,7 @@ public class UserProfile  extends AppCompatActivity{
         weight = w;
     }
 
-    public void calculateBMI(){
+    private void calculateBMI(){
         BMI = BMIconversionFactor*(weight/(height*height));     // 703*weight/height^2
         if (BMI > healthyBMImin && BMI < healthyBMImax)
             isHealthyBMI = true;
