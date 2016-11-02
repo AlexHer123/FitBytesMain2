10b77949437cd853ec2fc7446ce834974368e8f5
scaffi29 <scaffi29@uwm.edu>
2016-11-02 12:47:54 -0500

    userProfile work

--- app/src/main/java/com/example/alex/fitbytes/UserProfile.java
+++ app/src/main/java/com/example/alex/fitbytes/UserProfile.java
@@ -10,6 +10,9 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class UserProfile  extends AppCompatActivity{
-
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes);
+    }
 }
