ce3da3f9d142cd756f6728ff23843d2771f54642
gerthao <g.thao19@gmail.com>
2016-12-09 23:57:22 -0600

    Fixed some issues with Fitness Tracker.
    Added Notificatoins with Fitness Tracker when adding a Goal.  Not what I want in the end, but at least I can make notifications.

--- app/src/main/java/com/example/alex/fitbytes/DailyGoal.java
+++ app/src/main/java/com/example/alex/fitbytes/DailyGoal.java
@@ -9,29 +9,32 @@ import java.util.Calendar;
 public class DailyGoal extends Goal {
     private final int DAILY_DURATION = 1;
     public DailyGoal(){
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DATE);
+        cal.set(year, month, day);
+
         Category c = new Exercise();
         setCategory(c);
-        setDuration(DAILY_DURATION);
-        setDate(getUpdatedDate());
+        setDate(cal);
+        setDueDate(getUpdatedDate());
         setType(Type.DAILY);
     }
-    public DailyGoal(String description, int date, int duration){
+    public DailyGoal(String description, int date, int dueDate){
         Category c = new Exercise(description);
         setCategory(c);
         setDate(date);
-        setDuration(duration);
+        setDueDate(dueDate);
         setType(Type.DAILY);
     }
-    @Override
-    public String getDescription() {
-        if(super.getDescription().contains("Daily: "))
-            return super.getDescription();
-        else return "Daily: " + super.getDescription();
+    public String toString(){
+        return "Daily: " + getDescription() + (getCompleted()? " (Done)" : "");
     }
     private int getUpdatedDate(){
         Calendar c = Calendar.getInstance();
-        String dateString = String.format("%s%s%s", c.get(Calendar.YEAR),
-                (c.get(Calendar.MONTH)+1), c.get(Calendar.DATE));
+        String dateString = String.format("%04d%02d%02d", c.get(Calendar.YEAR),
+                (c.get(Calendar.MONTH)), c.get(Calendar.DATE));
         return Integer.parseInt(dateString);
     }
 }
